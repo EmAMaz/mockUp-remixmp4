874ebdb2194ae554f15d22ac9a14b5b08a4f6256
Santiago Maza <santiagomaza33@gmail.com>
2023-10-18 00:47:28 -0300

    bugs resueltos

--- src/app/components/my-user/my-user.component.ts
+++ src/app/components/my-user/my-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, Renderer2 } from '@angular/core';
+import { Observable, of } from "rxjs";
 
 @Component({
   selector: 'app-my-user',
@@ -8,24 +9,33 @@ import { Component, Renderer2 } from '@angular/core';
 export class MyUserComponent {
   public isOpen: boolean = true;
   public screenResolution?: string;
-  public itemListUser = [] = [
-    {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
-    {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
-    {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
-    {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
-    {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
-    {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
-    {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
-    {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
-    {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
-    {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
-    {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
-    {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"}
-  ]
+  public selectedItem: string = "Filtrar";
+  public dropdownActive: boolean = false;
+  public colActiva: boolean = true;
+  public filterOption: boolean = false;
+
+  public membresiaActive: boolean = true; 
+  public pagoActive: boolean = true; 
+  public precioActive: boolean = true; 
+  public itemListUser: any[] = []; 
 
   constructor(private renderer: Renderer2){}
   ngOnInit(){
     this.getScreenResolution();
+    this.itemListUser = [
+      {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
+      {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
+      {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
+      {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
+      {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
+      {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
+      {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
+      {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
+      {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
+      {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
+      {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"},
+      {fecha:"20/12/2023",nOrder:"0000037", membresia:"Plan basico", pago:"Paypal", precioTotal: "$20.00"}
+    ]
   }
   ngAfterViewInit() {
     this.renderer.listen('window', 'resize', () => {
@@ -40,9 +50,47 @@ export class MyUserComponent {
     }else{
       this.isOpen = true;
     }
+    if(window.innerWidth < 650){
+      this.colActiva = false;
+      this.filterOption = true;
+    }else{
+      this.filterOption = false;
+      this.colActiva = true;
+      this.membresiaActive = true; 
+      this.pagoActive = true; 
+      this.precioActive = true; 
+    }
+    
   }
   openMenu() {
     this.isOpen = !this.isOpen;
   }
+  selectCol($event:any){
+    if($event.target.innerHTML.toLowerCase() === "pago"){
+      this.selectedItem = $event.target.innerHTML;
+      this.dropdownActive = false;
+      this.precioActive = false;
+      this.membresiaActive = false;
+
+      this.pagoActive = true;
+    }else if($event.target.innerHTML.toLowerCase() === "plan membresia"){
+      this.selectedItem = $event.target.innerHTML;
+      this.dropdownActive = false;
+      this.precioActive = false;
+      this.pagoActive = false;
+
+      this.membresiaActive = true;
+    }else if($event.target.innerHTML.toLowerCase() === "precio total"){
+      this.selectedItem = $event.target.innerHTML;
+      this.dropdownActive = false;
+      this.membresiaActive = false;
+      this.pagoActive = false;
+
+      this.precioActive = true;
+    }else{
+      this.dropdownActive = !this.dropdownActive;
+    }
     
+  }
+  
 }
